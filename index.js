8e793fa474deb4e4af5dfa78f85d2ee176d041ec
gurgenidzegiorgi <gurgenidzegiorgi69@gmail.com>
2023-04-30 22:49:05 -0400

    modify index.js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const navBarMobile = document.querySelector('.mobile-menu-div');
 const hamburger = document.querySelector('.menu-btn');
 const closeBtn = document.querySelector('.close-btn');
-const check = document.querySelector('#check');
+
 const zero = document.querySelectorAll('.zero');
 const navList = document.querySelectorAll('li');
 const mobileBtn = document.querySelector('#mobile-menu-btn')
@@ -20,11 +20,7 @@ closeBtn.addEventListener('click', () => {
   toggleActiveClass([navBarMobile, hamburger, closeBtn]);
 });
 
-check.addEventListener('click', () => {
-  zero.forEach((element) => {
-    element.style.display = check.checked ? 'inline-block' : 'none';
-  });
-});
+
 
 navList.forEach((link) => link.addEventListener('click', () => {
   toggleActiveClass([navBarMobile, hamburger, closeBtn]);
